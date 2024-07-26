ca512623efda15b78a302aa3f8a55445233a4ccf
Francisco <francisco.cortes@mheducation.com>
2024-07-25 22:24:35 -0600

    Added login, register, and settings page, added  auth and main layout

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfilePictureComponent } from './components/profile-picture/profile-picture.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { ProfilePictureComponent } from './components/profile-picture/profile-pi
     ProfilePictureComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     ProfilePictureComponent
